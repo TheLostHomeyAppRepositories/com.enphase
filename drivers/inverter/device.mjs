b1756ce01a7626a41fb3d7e452a13ed3e8f371bb
Emile Nijssen <emile@athom.nl>
2025-04-07 15:59:04 +0200

    wip

--- drivers/inverter/device.mjs
+++ drivers/inverter/device.mjs
@@ -6,24 +6,23 @@ export default class EnphaseDeviceInverter extends EnphaseDevice {
     await super.onPoll();
 
     const siteData = await this.api.getSiteData();
-    const lifetimeEnergy = siteData?.module?.lifetime?.lifetimeEnergy?.value; // in Wh
-    console.log('lifetimeEnergy', lifetimeEnergy);
-    if (typeof lifetimeEnergy === 'number') {
-      await this.setCapabilityValue('meter_power', lifetimeEnergy / 1000)
+    const todayData = await this.api.getSiteToday();
+
+    const meterPower = siteData?.module?.lifetime?.lifetimeEnergy?.value; // in Wh
+    if (typeof meterPower === 'number') {
+      await this.setCapabilityValue('meter_power', meterPower / 1000)
         .catch(err => this.error('Error setting meter_power:', err));
     }
 
-    const todayData = await this.api.getSiteToday();
-    const latestPower = todayData?.latest_power?.value; // in W
-    if (typeof latestPower === 'number') {
-      await this.setCapabilityValue('measure_power', latestPower)
+    const measurePower = todayData?.latest_power?.value; // in W
+    if (typeof measurePower === 'number') {
+      await this.setCapabilityValue('measure_power', measurePower)
         .catch(err => this.error('Error setting measure_power:', err));
     }
 
-    const totalProductionToday = todayData?.stats?.[0]?.totals?.production;
-    console.log('totalProductionToday', totalProductionToday);
-    if (typeof totalProductionToday === 'number') {
-      await this.setCapabilityValue('meter_power.day', totalProductionToday / 1000)
+    const meterPowerDay = todayData?.stats?.[0]?.totals?.production;
+    if (typeof meterPowerDay === 'number') {
+      await this.setCapabilityValue('meter_power.day', meterPowerDay / 1000)
         .catch(err => this.error('Error setting meter_power.day:', err));
     }
 
